ef4122e26464a4f1f6740c94f1662fae2792ea53
Dave Dean <david.dean@hackney.gov.uk>
2023-08-17 10:28:19 +0100

    add store proc gateway

---
+++ RepairsListener/Gateway/Interfaces/IRepairsStoredProcedureGateway.cs
@@ -0,0 +1,14 @@
+using RepairsListener.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairsListener.Gateway.Interfaces
+{
+    public interface IRepairsStoredProcedureGateway
+    {
+        Task RunProcedure(string procName, params (string, string)[] parameters);
+    }
+}
---
+++ RepairsListener/Gateway/RepairsStoredProcedureGateway.cs
@@ -0,0 +1,38 @@
+using RepairsListener.Domain;
+using RepairsListener.Gateway.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairsListener.Gateway
+{
+    public class RepairsStoredProcedureGateway : IRepairsStoredProcedureGateway
+    {
+        public Task RunProcedure(string procName, params (string, string)[] parameters)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("REPAIRS_DB_CONNECTION_STRING");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(procName, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.AddWithValue(parameter.Item1, parameter.Item2);
+                    }
+
+                    var result = command.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
